30aaad144afec814ff78067e24cecbaa42cb01e4
Jamilie Souza <contato.jamiliesouza@gmail.com>
2023-11-10 22:14:56 -0300

    melhora no código para não exibir NaN

--- js/app.js
+++ js/app.js
@@ -6,5 +6,9 @@ button.addEventListener('click', function() {
 
     let result = Math.floor(Math.random() * (max - min + 1)) + min;
 
+    if(isNaN(result)) {
+        result = 'Valor Inválido';
+    }
+
     document.querySelector('.resultado > span').textContent = result;
 });
